e589f8fb7c995598770cf79070f4baedf2eadc7e
Wade Croft <wadecroft@gmail.com>
2022-11-18 13:44:39 -0800

    move lib-ts to lib

--- lib/Blocks.jsx
+++
@@ -1,9 +0,0 @@
-import Block from './Block'
-
-export default function Blocks({data, ...props}) {
-	return (
-		<>
-			{data?.map((block, i) => <Block key={i} block={block} {...props} /> )}
-		</>
-	)
-}
--- lib/coreBlocks/Embed.jsx
+++
@@ -1,9 +0,0 @@
-import ReactPlayer from "react-player";
-
-export default function Embed({block, className}) {
-    return (
-        <div className={className}>
-          <ReactPlayer url={block.data.attrs.url} width="100%" />
-        </div>
-    )
-}
