92d6f419183a726296d1afb6cc1660f40f760a22
Wade Croft <wadecroft@gmail.com>
2022-11-22 17:31:13 -0800

    update block config import

--- lib/hooks/useGlobalConfig.ts
+++ lib/hooks/useGlobalConfig.ts
@@ -1,12 +1,12 @@
 /* DEVELOPMENT:
 *   While testing locally, uncomment the below line and specify the absolute path to the next-wp config file of the project you're testing with via npm link
 */
-const blockConfig = require('C:/Users/Kaelan Smith/Documents/Stikky Media/Lionheart Coaching/website-2/tailwindui-pocket/next-wp.config.js');
+// const blockConfig = require('C:/Users/Kaelan Smith/Documents/Stikky Media/Lionheart Coaching/website-2/tailwindui-pocket/next-wp.config.js');
 
 /* FOR PRODUCTION:
 *   In production, we grab the next-wp config using a relative path, which assumes the user's node_modules folder is at the root level, and next-wp.config is also at the root level.. can improve this later to work in edge-case scenarios
 */
-// const blockConfig = require('../../../../next-wp.config.js'); // CommonJS version
+const blockConfig = require('../../../../next-wp.config.js'); // CommonJS version
 
 // Below, I tried dynamically using the imported config based on whether NODE_ENV == production or development.. but a "Module not found" error will always occur for one of the above imports, so I'm abandoning this for now and just manually commenting out the development/testing import 
 // const config = {
