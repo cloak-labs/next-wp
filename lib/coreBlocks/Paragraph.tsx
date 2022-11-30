1bc844c37a7fa988079d3f0468ef76c377f17b6c
Kaelan Smith <kaelancsmith@gmail.com>
2022-11-29 19:13:12 -0800

    Small fix

--- lib/coreBlocks/Paragraph.tsx
+++ lib/coreBlocks/Paragraph.tsx
@@ -3,19 +3,19 @@ import parse from 'html-react-parser'
 import classNames from '../utils/classNames';
 
 export default function Paragraph({block, className}) {
-    const {classes, styles} = useBlockStyleBuilder(block.data)
-    
-    return (        
-        <p
-            className={classNames(
-                "h-min-content", 
-                classes,
-                block?.data?.attrs?.backgroundColor ? "p-6" : 'pb-6',
-                className
-            )}
-            style={styles}
-        >
-            {parse(block.data.attrs.content)}
-        </p>
-    );
+  const {classes, styles} = useBlockStyleBuilder(block.data)
+  
+  return (        
+    <p
+      className={classNames(
+        "h-min-content", 
+        classes,
+        block?.data?.attrs?.backgroundColor ? "p-6" : 'pb-6',
+        className
+      )}
+      style={styles}
+    >
+      {parse(block.data.attrs.content)}
+    </p>
+  );
 }
