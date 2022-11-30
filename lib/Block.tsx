ed7c1bd41140439e9400ae9e0232c012210730bc
Kaelan Smith <kaelancsmith@gmail.com>
2022-11-29 19:10:07 -0800

    Add SmallContainer for certain blocks

--- lib/Block.tsx
+++ lib/Block.tsx
@@ -33,31 +33,34 @@ export default function Block({
     /*  
         next-wp provides simple/sensible defaults for core block components, the block container, 
         and the condition for which the block container is used (devs can override all these 
-        defaults via the Block component's props or the global config's props) 
+        defaults via the Block component's props or the global config's props)
     */
+    
+    const SmallContainer = ({block}) => <Container innerClassName={classNames("max-w-3xl lg:max-w-4xl", block.config.containerClasses)}>{block.rendered}</Container>
+
     const defaults = {
         container: ({block}) => <Container className={classNames("relative", block.config.containerClasses)}>{block.rendered}</Container>,
         containerCondition: ({block}) => block.isNested == false && block.config.container,
         blocks: {
             'core/paragraph': {
                 component: Paragraph,
-                container: true,
+                container: SmallContainer,
                 containerClasses: 'py-2',
             },
             'core/heading': {
                 component: Heading,
-                container: true,
+                container: SmallContainer,
                 containerClasses: 'py-2',
             },
             'core/image': {
                 component: Image,
-                container: true,
+                container: SmallContainer,
                 // container: ({block}) => (<Container className={classNames("max-w-2xl", block.config.containerClasses)}>{block.rendered}</Container>),
                 containerClasses: 'py-2',
             },
             'core/embed': {
                 component: Embed,
-                container: true,
+                container: SmallContainer,
                 containerClasses: 'py-2',
             },
             'core/html': {
@@ -79,7 +82,7 @@ export default function Block({
             },
             'core/list': {
                 component: List,
-                container: true,
+                container: SmallContainer,
             },
             'core/list-item': {
                 component: ListItem,
@@ -87,7 +90,7 @@ export default function Block({
             },
             'core/buttons': {
                 component: Buttons,
-                container: true,
+                container: SmallContainer,
                 containerClasses: 'py-2'
             },
             'core/button': {
