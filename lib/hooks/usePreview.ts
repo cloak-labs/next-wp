686d4a9fb5659807f5638c5a918d7a090cde806d
Kaelan Smith <kaelancsmith@gmail.com>
2022-12-01 08:55:56 -0800

    All usePreview to accept postType param so previews can work on all post types

--- lib/hooks/usePreview.ts
+++ lib/hooks/usePreview.ts
@@ -1,14 +1,14 @@
 import { useFetchRestAPI } from "../api/apiUtils";
 
-export async function usePreview(previewParams) {
-    const { id, parent, parentSlug } = previewParams.post
-    const data = await useFetchRestAPI(`/pages/${parent}/revisions/${id}`);
-    return {
-        data,
-        params: {
-            id,
-            parent,
-            parentSlug
-        }
+export async function usePreview(previewParams, postType = 'pages') {
+  const { id, parent, parentSlug } = previewParams.post
+  const data = await useFetchRestAPI(`/${postType}/${parent}/revisions/${id}`);
+  return {
+    data,
+    params: {
+      id,
+      parent,
+      parentSlug
     }
+  }
 }
