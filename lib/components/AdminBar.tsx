c2384e81a2739223391ef966cf4676d578858c85
Kaelan Smith <kaelancsmith@gmail.com>
2022-12-01 16:54:38 -0800

    Add AdminBar component which is currently just used for showing preview mode and ability to exit -- more admin utility to come

---
+++ lib/components/AdminBar.tsx
@@ -0,0 +1,18 @@
+import Button from '../components/Button';
+
+export function AdminBar({ previewParams }) {
+  let href = '/api/exit-preview';
+  if (previewParams) href = `/api/exit-preview?slug=${previewParams.frontEndSlug}`;
+  return (
+    <div className="w-full flex items-center justify-center bg-gray-900 text-gray-200 px-8 py-1.5">
+      <div className='flex'>
+        <p className="font-medium mb-0 text-base leading-7 flew-grow mr-4">
+          You are previewing unpublished changes.
+        </p>
+        <Button color='gray' linkProps={{prefetch: false}} className="py-1" href={href} size="small">
+          Exit Preview
+        </Button>
+      </div>
+    </div>
+  );
+}
