cc298a59e2b45fc1b1c02a28faa45103f258fe1a
Kaelan Smith <kaelancsmith@gmail.com>
2022-12-05 12:41:58 -0800

    Format + cleanup

--- lib/coreBlocks/Image.tsx
+++ lib/coreBlocks/Image.tsx
@@ -5,15 +5,18 @@ import ConditionalWrapper from '../components/ConditionalWrapper';
 import Link from '../components/Link'
 import { useBlockStyleBuilder } from '../hooks/useBlockStyleBuilder';
 
-export default function Image({block, className, quality = 75, priority = false, placeholder = 'empty', blurDataURL}) {
+export default function Image({
+  block,
+  className,
+  quality = 75,
+  priority = false,
+  placeholder = 'empty',
+  blurDataURL
+}) {
+
   let {classes, styles} = useBlockStyleBuilder(block.data)
   let { url, alt, caption, href, width, height, align, className: wpClassName } = block?.data?.attrs
   const captionColor = block?.parent?.attrs?.textColor || 'gray-700'
-
-  // width = !block.isNested && width ? width : '1300'
-  // height = !block.isNested && height ? height : '1300'
-  // width = !block.isNested && width ? width : '1300'
-  // height = !block.isNested && height ? height : '1300'
 
   styles = {
     ...styles,
@@ -22,23 +25,38 @@ export default function Image({block, className, quality = 75, priority = false,
   }
   
   return (
-    <div className={classNames('relative', align == 'center' && 'mx-auto', align == 'right' && 'ml-auto')} style={styles}>
+    <div
+      className={classNames(
+        'relative',
+        align == 'center' && 'mx-auto',
+        align == 'right' && 'ml-auto'
+      )}
+      style={styles}
+    >
       <Link href={href} className="relative">
         <NextImage
           src={url}
           className={classNames("rounded-lg", (block.isNested || width == 0) && "w-full h-full", wpClassName.includes("is-style-rounded") && "rounded-full", className, classes)}
+          width={width || 1300}
+          height={height}
           alt={alt}
-          // fill={true}
           quality={quality}
           priority={priority}
           placeholder={(placeholder == 'blur' && !blurDataURL) ? 'empty' : placeholder} // prevent user error by not allowing placeholder == 'blur' when user hasn't defined a blurDataURL
           blurDataURL={blurDataURL}
-          width={width || 1300}
-          height={height}
-          // layout="responsive"
         />
       </Link>
-      {caption && <p className={classNames('text-sm text-center mt-2', `text-${captionColor}`)} style={{width: width || '100%', maxWidth: "100%"}}>{parse(caption)}</p>}
+      {caption && 
+        <p
+          className={classNames(
+            'text-sm text-center mt-2',
+            `text-${captionColor}`
+          )} 
+          style={{width: width || '100%', maxWidth: "100%"}}
+        >
+          {parse(caption)}
+        </p>
+      }
     </div>
   )
 }
