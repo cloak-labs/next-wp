3aa3eb12ac31ceebf2b79cc0a012d794e17bc57b
Kaelan Smith <kaelancsmith@gmail.com>
2022-12-14 16:04:34 -0800

    Allow previews to work without post revisions

--- lib/hooks/usePreview.ts
+++ lib/hooks/usePreview.ts
@@ -1,8 +1,15 @@
 import { useFetchRestAPI } from "./useFetchRestAPI";
 
 export async function usePreview(previewParams) {
-  const { revisionId, postId, postTypeRestEndpoint, ...rest } = previewParams.post
-  const data = await useFetchRestAPI(`/${postTypeRestEndpoint}/${postId}/revisions/${revisionId}`);
+  const { revisionId = null, postId, postTypeRestEndpoint, ...rest } = previewParams.post
+
+  let data
+  if(revisionId){
+    data = await useFetchRestAPI(`/${postTypeRestEndpoint}/${postId}/revisions/${revisionId}`);
+  }else{
+    data = await useFetchRestAPI(`/${postTypeRestEndpoint}/${postId}`);
+  }
+
   return {
     data,
     params: {
