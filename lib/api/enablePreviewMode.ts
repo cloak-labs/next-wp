60264336a78087264955bcb57e37e6d55faa8b38
Kaelan Smith <kaelancsmith@gmail.com>
2022-12-14 16:02:41 -0800

    Add fix for postId == 0

--- lib/api/enablePreviewMode.ts
+++ lib/api/enablePreviewMode.ts
@@ -5,7 +5,12 @@ import { useSlugModifier } from "../hooks/useSlugModifier";
 export default async function enablePreviewMode(req, res) {
   const config = await useGlobalConfig()
 
-  const { secret, revisionId, postId, postType } = req.query;
+  let { secret, revisionId, postId, postType } = req.query;
+
+  if(postId == 0 || postId == '0') {
+    postId = `${revisionId}` // this fixes previewing a draft or a post that doesn't have a revision.. string interpolation is used to *copy* revisionId rather than reference it
+    revisionId = null
+  }
 
   // Check the secret and next parameters.
   // This secret should only be known by this API route
@@ -16,7 +21,7 @@ export default async function enablePreviewMode(req, res) {
     return res.status(401).json({ message: 'Invalid secret token -- pass in a valid secret via a "secret" parameter that matches the secret you supplied as "wpSecret" in your next-wp.config.js file.' });
   }
  
-  if (!revisionId) return res.status(401).json({ message: 'A post revision ID was not supplied -- pass it in via a "revisionId" parameter.' });
+  // if (!revisionId) return res.status(401).json({ message: 'A post revision ID was not supplied -- pass it in via a "revisionId" parameter.' });
   if (!postId) return res.status(401).json({ message: 'A master post ID was not supplied -- pass it in via a "postId" parameter.' });
   if (!postType) return res.status(401).json({ message: 'A post type was not supplied -- pass it in via a "postType" parameter.' });
 
